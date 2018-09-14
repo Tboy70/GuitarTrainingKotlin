8380fc04baeaaad1afee5bd2708d821371180b92
Thomas Boy <thomas.boy@orange.fr>
2018-09-14 18:05:39 +0200

    Cleaned code + design.

--- presentation/src/main/java/thomas/example/com/guitarTrainingKotlin/fragment/login/CreateAccountFragment.kt
+++ presentation/src/main/java/thomas/example/com/guitarTrainingKotlin/fragment/login/CreateAccountFragment.kt
@@ -1,5 +1,6 @@
 package thomas.example.com.guitarTrainingKotlin.fragment.login
 
+import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProvider
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
@@ -8,6 +9,8 @@ import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.fragment_create_account.*
 import thomas.example.com.guitarTrainingKotlin.R
+import thomas.example.com.guitarTrainingKotlin.component.ErrorRendererComponent
+import thomas.example.com.guitarTrainingKotlin.component.MaterialDialogComponent
 import thomas.example.com.guitarTrainingKotlin.fragment.BaseFragment
 import thomas.example.com.guitarTrainingKotlin.viewmodel.login.CreateAccountViewModel
 import javax.inject.Inject
@@ -18,6 +21,12 @@ class CreateAccountFragment : BaseFragment() {
     lateinit var viewModelFactory: ViewModelProvider.Factory
     private lateinit var createAccountViewModel: CreateAccountViewModel
 
+    @Inject
+    lateinit var errorRendererComponent: ErrorRendererComponent
+
+    @Inject
+    lateinit var materialDialogComponent: MaterialDialogComponent
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_create_account, container, false)
     }
@@ -27,11 +36,36 @@ class CreateAccountFragment : BaseFragment() {
 
         createAccountViewModel = ViewModelProviders.of(this, viewModelFactory).get(CreateAccountViewModel::class.java)
 
+        handleLiveData(view)
         handleClickValidateCreation()
+    }
+
+    private fun handleLiveData(view: View) {
+        createAccountViewModel.finishLoading.observe(this, Observer<Boolean> {
+            if (it != null) {
+                materialDialogComponent.dismissDialog()
+            }
+        })
+
+        createAccountViewModel.creationSuccess.observe(this, Observer<Boolean> {
+            if (it != null && it == true) {
+                fragmentManager?.popBackStack()
+            }
+        })
+
+        createAccountViewModel.creationFailure.observe(this, Observer<Boolean> {
+            if (it != null && it == true) {
+                materialDialogComponent.dismissDialog()
+                if (createAccountViewModel.errorThrowable != null) {
+                    errorRendererComponent.requestRenderError(createAccountViewModel.errorThrowable as Throwable, ErrorRendererComponent.ERROR_DISPLAY_MODE_SNACKBAR, view)
+                }
+            }
+        })
     }
 
     private fun handleClickValidateCreation() {
         create_account_validate.setOnClickListener {
+            materialDialogComponent.showProgressDialog(getString(R.string.dialog_create_account_title), getString(R.string.dialog_create_account_content), R.color.colorPrimary)
             createAccountViewModel.createNewUser(create_account_pseudo.text.toString(), create_account_email.text.toString(), create_account_password.text.toString())
         }
     }
--- presentation/src/main/java/thomas/example/com/guitarTrainingKotlin/viewmodel/login/CreateAccountViewModel.kt
+++ presentation/src/main/java/thomas/example/com/guitarTrainingKotlin/viewmodel/login/CreateAccountViewModel.kt
@@ -1,11 +1,18 @@
 package thomas.example.com.guitarTrainingKotlin.viewmodel.login
 
+import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
 import thomas.example.com.interactor.user.CreateNewUser
 import thomas.example.com.model.User
 import javax.inject.Inject
 
 class CreateAccountViewModel @Inject constructor(private val createNewUser: CreateNewUser) : ViewModel() {
+
+    val finishLoading: MutableLiveData<Boolean> = MutableLiveData()
+    val creationSuccess: MutableLiveData<Boolean> = MutableLiveData()
+    val creationFailure: MutableLiveData<Boolean> = MutableLiveData()
+
+    var errorThrowable: Throwable? = null
 
     fun createNewUser(pseudoUser: String, emailUser: String, passwordUser: String) {
         val user = User()
@@ -15,14 +22,14 @@ class CreateAccountViewModel @Inject constructor(private val createNewUser: Crea
 
         createNewUser.execute(
                 onComplete = {
-
+                    finishLoading.postValue(true)
                 },
                 onError = {
-
+                    errorThrowable = it
+                    creationFailure.postValue(true)
                 },
                 onNext = {
-
-
+                    creationSuccess.postValue(true)
                 }, params = CreateNewUser.Params.toCreate(user))
     }
 
